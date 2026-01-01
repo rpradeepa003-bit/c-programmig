68b3452a695a68a606b2bdd5b103402607af8fb5
rpradeepa003-bit <rpradeepa003@gmail.com>
2026-01-01 21:45:36 +0530

    c program

---
+++ c program 23.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int num, reverse = 0, remainder;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    for (; num != 0; num /= 10) {
+        remainder = num % 10;
+        reverse = reverse * 10 + remainder;
+    }
+
+    printf("Reversed number: %d\\n", reverse);
+
+    return 0;
+}
