c7f95651f150286bf3c76091da2f2ddce101823d
rpradeepa003-bit <rpradeepa003@gmail.com>
2026-01-04 21:38:28 +0530

    c programming

---
+++ c program 50.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+    char str[100];
+    int count = 0;
+    int i;
+    printf("Enter a string: ");
+    gets(str);
+    for (i = 0; str[i] != '\0'; i++) {
+        if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u' ||
+            str[i] == 'A' || str[i] == 'E' || str[i] == 'I' || str[i] == 'O' || str[i] == 'U') {
+            count++;
+        }
+    }
+    printf("Number of vowels: %d\n", count);
+    return 0;
+}
