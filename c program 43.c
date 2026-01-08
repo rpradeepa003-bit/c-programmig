48d68833e64325c81d15f3bd8641836fa3483cf6
rpradeepa003-bit <rpradeepa003@gmail.com>
2026-01-08 21:45:20 +0530

    c programming

---
+++ c program 43.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+    int main() {
+    int size;
+    scanf("%d", &size);
+    int arr[size];
+    for(int i=0; i<size; i++){ 
+        scanf("%d", &arr[i]);
+    }
+    for(int i=0; i<size; i++){
+        for(int j=0; j<size-1; j++){
+            if(arr[j]<arr[j+1]){
+                int temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+        }    
+        
+    }
+    for(int i=0;i<size;i++){
+        printf("%d " ,arr[i]);
+    }
+    return 0;
+}
